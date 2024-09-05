4ba0473426dff02dbdbdedd17bce4502c012ed30
AmirHossein Norbakhsh <amircr7h@icloud.com>
2024-09-05 16:38:38 +0330

    developed and debugged my-post page completely

--- app/my-posts/page.tsx
+++ app/my-posts/page.tsx
@@ -7,22 +7,12 @@ import PostCard from '@/components/PostCard/PostCard';
 import viewImage from '@/assets/images/View1.png'
 import authorImage from '@/assets/images/Image1.png'
 import Link from 'next/link';
-
-interface Post {
-    _id: string;
-    category: string;
-    title: string;
-    content: string;
-    authorName: string;
-    publishDate: string;
-}
-
+import { Post } from '@/types/types';
 
 export default function MyPosts() {
 
     const [posts, setPosts] = useState<Post[]>([]);
     const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
 
     const fetchPosts = async () => {
         const token = localStorage.getItem('token');
@@ -40,7 +30,7 @@ export default function MyPosts() {
             const data = await response.json();
             setPosts(data);
         } catch (err) {
-            setError('Failed to load posts');
+            console.error(err)
         } finally {
             setLoading(false);
         }
@@ -53,6 +43,10 @@ export default function MyPosts() {
     if (loading) {
         return <div>Loading...</div>;
     }
+
+    // Sorting posts in descending order based on their publishDate.
+    posts.sort((a: { publishDate: string | number | Date; }, b: { publishDate: string | number | Date; }) => new Date(b.publishDate).getTime() - new Date(a.publishDate).getTime());
+
 
     return (
         <main className={styles.main}>
@@ -68,24 +62,24 @@ export default function MyPosts() {
                 </h1>
 
                 <div className={styles.posts}>
-                {posts.map((item) => (
-                    <PostCard
-                        key={item._id}
-                        imageUrl={viewImage}
-                        category="Technology"
-                        title={item.title}
-                        description={item.content}
-                        authorImageUrl={authorImage}
-                        authorName={item.author.name}
-                        date={new Date(item.publishDate).toLocaleDateString('en-US', {
-                            year: 'numeric',
-                            month: 'long',
-                            day: 'numeric',
-                        })}
-                        id={item._id}
-                        editable
-                    />
-                ))}
+                    {posts.map((item) => (
+                        <PostCard
+                            key={item._id}
+                            imageUrl={viewImage}
+                            category="Technology"
+                            title={item.title}
+                            description={item.content}
+                            authorImageUrl={authorImage}
+                            authorName={item.author.name}
+                            date={new Date(item.publishDate).toLocaleDateString('en-US', {
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric',
+                            })}
+                            id={item._id}
+                            editable
+                        />
+                    ))}
                 </div>
             </div>
             <Footer />
